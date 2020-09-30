74fedf4aa118f8a6a5e19757348d0b1d46f6a6ca
DucJS <mrtranduc1994@outlook.com>
2020-09-30 14:15:59 +0700

    scroll app bar header

--- src/features/NavBarHeader/index.jsx
+++ src/features/NavBarHeader/index.jsx
@@ -5,10 +5,13 @@ import {
   IconButton,
   Toolbar,
   Typography,
+  useScrollTrigger,
+  Slide,
 } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import logo from "../../assets/images/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg";
 import TableList from "../../components/TableList";
+import PropTypes from "prop-types";
 
 const movieDropdown = [
   { title: "Popular", link: "/movies/popular" },
@@ -24,7 +27,9 @@ const tvShowsDropdown = [
   { title: "Top Rated", link: "/tv-shows/top-rated" },
 ];
 
-const peopleDropdown = [{ title: "Popular People", link: "/people/popular-people" }];
+const peopleDropdown = [
+  { title: "Popular People", link: "/people/popular-people" },
+];
 
 const moreDropdown = [
   { title: "Discussions", link: "/more/discussions" },
@@ -33,99 +38,117 @@ const moreDropdown = [
   { title: "Api", link: "/more/api" },
 ];
 
-export default function NavBarHeader() {
+function HideOnScroll(props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({ target: window ? window() : undefined });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+HideOnScroll.propTypes = {
+  children: PropTypes.element.isRequired,
+  window: PropTypes.func,
+};
+
+export default function NavBarHeader(props) {
   const [active, setActive] = useState(false);
   return (
     <div className="navbar-header">
       <Container>
-        <AppBar position="fixed">
-          <Toolbar>
-            <IconButton className="navbar-header__logo">
-              <img src={logo} alt="logo-app" />
-            </IconButton>
-            <div className="navbar-header__menu">
-              <div
-                onMouseMove={() => {
-                  setActive("movieDropdown");
-                }}
-                onMouseLeave={() => {
-                  setActive(false);
-                }}
-                className="navbar-header__menu-item"
-              >
-                <Typography className="navbar-header__menu-item-title">
-                  Movies
-                </Typography>
-                {active === "movieDropdown" && (
-                  <div className="navbar-header__menu-item--dropdown">
-                    <TableList list={movieDropdown} />
-                  </div>
-                )}
+        <HideOnScroll {...props}>
+          <AppBar>
+            <Toolbar>
+              <IconButton className="navbar-header__logo">
+                <img src={logo} alt="logo-app" />
+              </IconButton>
+              <div className="navbar-header__menu">
+                <div
+                  onMouseMove={() => {
+                    setActive("movieDropdown");
+                  }}
+                  onMouseLeave={() => {
+                    setActive(false);
+                  }}
+                  className="navbar-header__menu-item"
+                >
+                  <Typography className="navbar-header__menu-item-title">
+                    Movies
+                  </Typography>
+                  {active === "movieDropdown" && (
+                    <div className="navbar-header__menu-item--dropdown">
+                      <TableList list={movieDropdown} />
+                    </div>
+                  )}
+                </div>
+                <div
+                  onMouseMove={() => {
+                    setActive("tvShowsDropdown");
+                  }}
+                  onMouseLeave={() => {
+                    setActive(false);
+                  }}
+                  className="navbar-header__menu-item"
+                >
+                  <Typography className="navbar-header__menu-item-title">
+                    TVShows
+                  </Typography>
+                  {active === "tvShowsDropdown" && (
+                    <div className="navbar-header__menu-item--dropdown">
+                      <TableList list={tvShowsDropdown} />
+                    </div>
+                  )}
+                </div>
+                <div
+                  onMouseMove={() => {
+                    setActive("peopleDropdown");
+                  }}
+                  onMouseLeave={() => {
+                    setActive(false);
+                  }}
+                  className="navbar-header__menu-item"
+                >
+                  <Typography className="navbar-header__menu-item-title">
+                    People
+                  </Typography>
+                  {active === "peopleDropdown" && (
+                    <div className="navbar-header__menu-item--dropdown">
+                      <TableList list={peopleDropdown} />
+                    </div>
+                  )}
+                </div>
+                <div
+                  onMouseMove={() => {
+                    setActive("moreDropdown");
+                  }}
+                  onMouseLeave={() => {
+                    setActive(false);
+                  }}
+                  className="navbar-header__menu-item"
+                >
+                  <Typography className="navbar-header__menu-item-title">
+                    More
+                  </Typography>
+                  {active === "moreDropdown" && (
+                    <div className="navbar-header__menu-item--dropdown">
+                      <TableList list={moreDropdown} />
+                    </div>
+                  )}
+                </div>
               </div>
-              <div
-                onMouseMove={() => {
-                  setActive("tvShowsDropdown");
-                }}
-                onMouseLeave={() => {
-                  setActive(false);
-                }}
-                className="navbar-header__menu-item"
-              >
-                <Typography className="navbar-header__menu-item-title">
-                  TVShows
-                </Typography>
-                {active === "tvShowsDropdown" && (
-                  <div className="navbar-header__menu-item--dropdown">
-                    <TableList list={tvShowsDropdown} />
-                  </div>
-                )}
-              </div>
-              <div
-                onMouseMove={() => {
-                  setActive("peopleDropdown");
-                }}
-                onMouseLeave={() => {
-                  setActive(false);
-                }}
-                className="navbar-header__menu-item"
-              >
-                <Typography className="navbar-header__menu-item-title">
-                  People
-                </Typography>
-                {active === "peopleDropdown" && (
-                  <div className="navbar-header__menu-item--dropdown">
-                    <TableList list={peopleDropdown} />
-                  </div>
-                )}
-              </div>
-              <div
-                onMouseMove={() => {
-                  setActive("moreDropdown");
-                }}
-                onMouseLeave={() => {
-                  setActive(false);
-                }}
-                className="navbar-header__menu-item"
-              >
-                <Typography className="navbar-header__menu-item-title">
-                  More
-                </Typography>
-                {active === "moreDropdown" && (
-                  <div className="navbar-header__menu-item--dropdown">
-                    <TableList list={moreDropdown} />
-                  </div>
-                )}
-              </div>
-            </div>
-            <Typography>Login</Typography>
-            <IconButton>
-              <SearchIcon
-                fontSize="large"
-                className="navbar-header__search-icon"
-              />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+              <Typography>Login</Typography>
+              <IconButton>
+                <SearchIcon
+                  fontSize="large"
+                  className="navbar-header__search-icon"
+                />
+              </IconButton>
+            </Toolbar>
+          </AppBar>
+        </HideOnScroll>
       </Container>
     </div>
   );
