a4430112ab4ac899f23f7311fdf52f31691dd8d8
DucJS <mrtranduc1994@outlook.com>
2020-10-05 02:23:28 +0700

    use react.lazy

--- src/features/Peoples/index.jsx
+++ src/features/Peoples/index.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Route } from "react-router-dom";
-import PeoplePreview from "./pages/PeoplePreview";
 import PopularPage from "./pages/PopularPage";
 
+const PeoplePreview = React.lazy(() => import("./pages/PeoplePreview"));
+
 export default function Peoples() {
-    return (
-        <div>
-            <Route exact path="/peoples/popular-peoples" >
-                <PopularPage />
-            </Route>
-            <Route exact path="/peoples/:id">
-                <PeoplePreview />
-            </Route>
-        </div>
-    )
+  return (
+    <div>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route exact path="/peoples/popular-peoples">
+          <PopularPage />
+        </Route>
+        <Route exact path="/peoples/:id" component={PeoplePreview} />
+      </Suspense>
+    </div>
+  );
 }
