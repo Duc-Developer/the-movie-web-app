d1a8a63f02434dae27362cb77e17e907f90af148
DucJS <mrtranduc1994@outlook.com>
2020-10-08 06:56:31 +0700

    create adornment for type password

--- src/fields/TextFieldController/index.jsx
+++ src/fields/TextFieldController/index.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Controller } from "react-hook-form";
 import TextField from "@material-ui/core/TextField";
 import InputAdornment from "@material-ui/core/InputAdornment";
-import { Grid, Typography } from "@material-ui/core";
+import { Grid, IconButton, Typography } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
 import PropTypes from "prop-types";
 
 TextFieldController.propTypes = {
@@ -11,12 +13,26 @@ TextFieldController.propTypes = {
   name: PropTypes.string,
   rules: PropTypes.object,
   icon: PropTypes.node,
-  type: PropTypes.string
+  type: PropTypes.string,
+  enableAdornment: PropTypes.bool,
+};
+
+TextFieldController.defaultProps = {
+  enableAdornment: false,
+  type: "text",
 };
 
 export default function TextFieldController(props) {
-  const { control, errors, name, rules, icon, type } = props;
+  const { control, errors, name, rules, icon, type, enableAdornment } = props;
+  const [fieldType, setFieldType] = useState(type);
 
+  const handleClickShowPassword = () => {
+    setFieldType("text");
+  };
+
+  const handleMouseDownPassword = () => {
+    setFieldType("password");
+  };
   return (
     <Controller
       control={control}
@@ -27,13 +43,30 @@ export default function TextFieldController(props) {
           <Grid item xs={12}>
             <TextField
               label={name}
-              type={type}
+              type={fieldType}
+              fullWidth
               onChange={(e) => {
                 onChange(e.target.value);
               }}
               InputProps={{
                 startAdornment: (
                   <InputAdornment position="start">{icon}</InputAdornment>
+                ),
+                endAdornment: enableAdornment && (
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {fieldType === "text" ? (
+                        <Visibility />
+                      ) : (
+                        <VisibilityOff />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
                 ),
               }}
             />
