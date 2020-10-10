525b079aefcec9e845bc7130b2bcea1228dc5869
DucJS <mrtranduc1994@outlook.com>
2020-10-10 21:11:42 +0700

    Updates

---
+++ precache-manifest.a883bc32d27ad3c258acb655cd2d87c0.js
@@ -0,0 +1,133 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e0a292fc58266b7e6231ce6403563ef",
+    "url": "/the-movie-web-app/index.html"
+  },
+  {
+    "revision": "b81ded157f6d5ed5e27e",
+    "url": "/the-movie-web-app/static/css/main.ee21d9fe.chunk.css"
+  },
+  {
+    "revision": "aeb6978c89f76b81e77d",
+    "url": "/the-movie-web-app/static/js/0.27763349.chunk.js"
+  },
+  {
+    "revision": "2d024ebf42e69151626c",
+    "url": "/the-movie-web-app/static/js/1.14c6ba4d.chunk.js"
+  },
+  {
+    "revision": "4cca3bb84dd60fbe6228",
+    "url": "/the-movie-web-app/static/js/10.71274c95.chunk.js"
+  },
+  {
+    "revision": "e7e644d317ad64192522",
+    "url": "/the-movie-web-app/static/js/11.1937e6b3.chunk.js"
+  },
+  {
+    "revision": "5f9c9b9003261a853753",
+    "url": "/the-movie-web-app/static/js/12.f9fd514a.chunk.js"
+  },
+  {
+    "revision": "7091ce32aed5e296f109",
+    "url": "/the-movie-web-app/static/js/4.c533846e.chunk.js"
+  },
+  {
+    "revision": "abfa9001b4087bb9e9a892e4db297d32",
+    "url": "/the-movie-web-app/static/js/4.c533846e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "764606306694eedf359d",
+    "url": "/the-movie-web-app/static/js/5.8ba26824.chunk.js"
+  },
+  {
+    "revision": "6da4b2da8929c3dd3573",
+    "url": "/the-movie-web-app/static/js/6.b46e1211.chunk.js"
+  },
+  {
+    "revision": "783f14fa45b10e088e68f98251448010",
+    "url": "/the-movie-web-app/static/js/6.b46e1211.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7793160cfaddb9ce771b",
+    "url": "/the-movie-web-app/static/js/7.8b74e596.chunk.js"
+  },
+  {
+    "revision": "42821fdf108b5d319574",
+    "url": "/the-movie-web-app/static/js/8.6ca9d200.chunk.js"
+  },
+  {
+    "revision": "67d97ce6e2aed49da8bc",
+    "url": "/the-movie-web-app/static/js/9.a6741096.chunk.js"
+  },
+  {
+    "revision": "b81ded157f6d5ed5e27e",
+    "url": "/the-movie-web-app/static/js/main.ee993ccb.chunk.js"
+  },
+  {
+    "revision": "967aafb42e83f3b8c682",
+    "url": "/the-movie-web-app/static/js/runtime-main.f92dba23.js"
+  },
+  {
+    "revision": "48f1944af0062d7444f0f760196a78fb",
+    "url": "/the-movie-web-app/static/media/023eb1965f0623f9fc3b975fa8dba6bb.48f1944a.gif"
+  },
+  {
+    "revision": "0af10deb0b7c3b70e8911a7fc177b71a",
+    "url": "/the-movie-web-app/static/media/2cae3488025585.5dca0b4379d96.0af10deb.gif"
+  },
+  {
+    "revision": "74ff0bfbd73f81a7e4b5c2d9dad1510f",
+    "url": "/the-movie-web-app/static/media/2d355e9108415af382c18013fd626e8e.74ff0bfb.gif"
+  },
+  {
+    "revision": "f2012686c0ac3e51b7263d6438129a78",
+    "url": "/the-movie-web-app/static/media/3JGwuPUY7BB_.f2012686.gif"
+  },
+  {
+    "revision": "c35441a5a3e22a0b317984bb273134c7",
+    "url": "/the-movie-web-app/static/media/404-error-page-templates.c35441a5.jpg"
+  },
+  {
+    "revision": "ee6a3d0e75f75d562b1b907db0a40da9",
+    "url": "/the-movie-web-app/static/media/494641-PHESOC-447.ee6a3d0e.png"
+  },
+  {
+    "revision": "e3edb7fefa634f81e8e19e6e340f6457",
+    "url": "/the-movie-web-app/static/media/Screenshot from 2020-10-01 21-02-16.e3edb7fe.jpg"
+  },
+  {
+    "revision": "f620e5fdb9a2e0b60d45c119586761e4",
+    "url": "/the-movie-web-app/static/media/avatar.f620e5fd.svg"
+  },
+  {
+    "revision": "374a04af25e7376c1926c16afa34974f",
+    "url": "/the-movie-web-app/static/media/bg.374a04af.svg"
+  },
+  {
+    "revision": "56fd61eec148690af48f876dfd5c8718",
+    "url": "/the-movie-web-app/static/media/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.56fd61ee.svg"
+  },
+  {
+    "revision": "24a6fdb93a3bb0275107bfc71637cb21",
+    "url": "/the-movie-web-app/static/media/blue_square_2-d537fb228cf3ded904ef09b136fe3fec72548ebc1fea3fbbd1ad9e36364db38b.24a6fdb9.svg"
+  },
+  {
+    "revision": "cb2a80ffb31e5ecb75b8bf724ff686cf",
+    "url": "/the-movie-web-app/static/media/dc5b310af8ced207029634c08a538cae.cb2a80ff.gif"
+  },
+  {
+    "revision": "31c9688ce4881fff3ebb21d333759d65",
+    "url": "/the-movie-web-app/static/media/default_avatar.31c9688c.png"
+  },
+  {
+    "url": "/the-movie-web-app/static/media/e4c6409d1a3621b57cfe404056e5faa3.e4c6409d.gif"
+  },
+  {
+    "revision": "1fc9df1a016530f46fa5b11e5813a923",
+    "url": "/the-movie-web-app/static/media/no_poster.1fc9df1a.jpg"
+  },
+  {
+    "revision": "6d252a00c562aee3ae3bab8ed4658f52",
+    "url": "/the-movie-web-app/static/media/wave.6d252a00.png"
+  }
+]);
